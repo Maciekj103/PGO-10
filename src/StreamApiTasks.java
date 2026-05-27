95fa4245017c4035c0ceac3d2c968dd2d7195a1e
Maciekj103 <maciek.jablkowski06@gmail.com>
2026-05-27 09:35:40 +0200

    Zrobiono drugie zadanie

--- src/StreamApiTasks.java
+++ src/StreamApiTasks.java
@@ -83,8 +83,10 @@ public class StreamApiTasks {
     }
 
     static List<Order> ordersAbove(List<Order> orders, double minValue) {
-        // TODO: zadanie 2
-        return List.of();
+        return orders.stream()
+                .filter(order -> order.totalValue() > minValue)
+                .sorted(Comparator.comparingDouble(Order::totalValue).reversed())
+                .toList();
     }
 
     static List<String> uniqueCustomerNames(List<Order> orders) {
